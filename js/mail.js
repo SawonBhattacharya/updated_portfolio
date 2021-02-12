7fbac6e221b782e449a1b2b261139ef64bd2152f
Sawon Bhattacharya <sawon17081997@gmail.com>
2021-02-12 17:08:43 +0530

    Update11

--- js/mail.js
+++ js/mail.js
@@ -1,12 +1,14 @@
 function sendEmail() { 
     Email.send({ 
-      SecurityToken:"a5639d4b-9814-4c5a-9395-55dfabff39d0",
+      Host: "smtp.gmail.com",
+      Username: "sawon17081997@gmail.com",
+      Password:"5955F329597588C2F1B6AD4060E52DD294E2",
       To: "bhattasawon17@gmail.com",
       Name: 'Sawon',//document.getElementById('contactName').value,
       From: 'sawon17081997@gmail.com',//document.getElementById('contactEmail').value,
       Subject:'Work',//document.getElementById('contactSubject').value,
       Body: 'Lets do this',//document.getElementById('contactMessage').value
-    }) 
+    })
     .then(function (message) { 
       alert("mail sent successfully") 
     }); 
